10357a29abdb081566ff1f0d4ef9b1b3ebc02f84
KaneTheDev <lehoaituankhang@gmail.com>
2026-07-18 17:59:45 +0700

    Adjusted Camera

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [Header("Movement")]
-    public float speed;
+    public float speed = 8f;
+    [SerializeField] float acceleration = 12f;
+    [SerializeField] float deceleration = 16f;
     public float movementX, movementZ, factor;
     public bool movable, targetReached;
     public Transform root, target;
     Rigidbody rb;
     [SerializeField] float restrictedRadius;
+    private Vector3 currentVelocity;
 
     [Header("Shake Settings")]
     [SerializeField] float duration = 0.4f;
@@ -56,11 +59,20 @@ public class CameraMovement : MonoBehaviour
 
         movementX = Input.GetAxisRaw("Horizontal");
         movementZ = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(movementX, 0f, movementZ).normalized;
-        rb.velocity = direction * speed * factor;
+        Vector3 inputDirection = new Vector3(movementX, 0f, movementZ).normalized;
 
-        // if (Input.GetKeyDown(KeyCode.B)) 
-            // StartCoroutine(ScreenShake(0.2f));
+        Vector3 projectedForward = Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized;
+        Vector3 projectedRight = Vector3.ProjectOnPlane(transform.right, Vector3.up).normalized;
+
+        Vector3 moveDirection = (projectedForward * inputDirection.z + projectedRight * inputDirection.x);
+        Vector3 targetVelocity = moveDirection * speed * factor;
+
+        if (inputDirection.sqrMagnitude > 0.001f)
+            currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, Time.deltaTime * acceleration);
+        else
+            currentVelocity = Vector3.Lerp(currentVelocity, Vector3.zero, Time.deltaTime * deceleration);
+
+        rb.velocity = currentVelocity;
     }
 
     public void ScreenShake(float amplitude)
